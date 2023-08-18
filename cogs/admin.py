819c2b7c3107e6cbdb1029cfe6e7526aba941d97
STACIA <69411527+staciax@users.noreply.github.com>
2023-08-18 20:33:31 +0700

    cogs

---
+++ cogs/admin.py
@@ -0,0 +1,115 @@
+from __future__ import annotations
+
+import logging
+from typing import TYPE_CHECKING, Literal
+
+import discord
+from discord import Embed, app_commands
+from discord.app_commands import locale_str as _T
+from discord.ext import commands
+
+from core.checks import bot_has_permissions, owner_only
+
+if TYPE_CHECKING:
+    from core.bot import Lunaria
+
+
+_log = logging.getLogger(__name__)
+
+EXTENSIONS = Literal['cogs.jsk',]
+
+
+class Admin(commands.Cog, name='admin'):
+    """Admin commands"""
+
+    def __init__(self, bot: Lunaria) -> None:
+        self.bot = bot
+
+    extension = app_commands.Group(
+        name=_T('ext'),
+        description=_T('extension manager'),
+        default_permissions=discord.Permissions(
+            administrator=True,
+        ),
+        guild_only=True,
+    )
+
+    @extension.command(name=_T('load'), description=_T('Load an extension'))
+    @app_commands.describe(extension=_T('extension name'))
+    @app_commands.rename(extension=_T('extension'))
+    @bot_has_permissions(send_messages=True, embed_links=True)
+    @owner_only()
+    async def extension_load(self, interaction: discord.Interaction[Lunaria], extension: Literal[EXTENSIONS]) -> None:
+        await interaction.response.defer(ephemeral=True)
+        await self.bot.load_extension(f'{extension}')
+
+        embed = Embed(description=f"**Loaded**: `{extension}`")  # .success()
+        await interaction.followup.send(embed=embed, silent=True)
+
+    @extension.command(name=_T('unload'), description=_T('Unload an extension'))
+    @app_commands.describe(extension=_T('extension name'))
+    @app_commands.rename(extension=_T('extension'))
+    @bot_has_permissions(send_messages=True, embed_links=True)
+    @owner_only()
+    async def extension_unload(self, interaction: discord.Interaction[Lunaria], extension: EXTENSIONS) -> None:
+        await interaction.response.defer(ephemeral=True)
+        await self.bot.unload_extension(f'{extension}')
+
+        embed = Embed(description=f'**Unloaded**: `{extension}`')  # .success()
+        await interaction.followup.send(embed=embed, silent=True)
+
+    @extension.command(name=_T('reload'))
+    @app_commands.describe(extension=_T('extension name'))
+    @app_commands.rename(extension=_T('extension'))
+    @bot_has_permissions(send_messages=True, embed_links=True)
+    @owner_only()
+    async def extension_reload(self, interaction: discord.Interaction[Lunaria], extension: EXTENSIONS) -> None:
+        await interaction.response.defer(ephemeral=True)
+
+        await self.bot.reload_extension(f'{extension}')
+
+        embed = Embed(description=f"**Reloaded**: `{extension}`")  # .success()
+        await interaction.followup.send(embed=embed, silent=True)
+
+    @app_commands.command(name='sync', description='Syncs the application commands to Discord.')
+    @app_commands.rename(guild_id=_T('guild_id'))
+    @app_commands.describe(guild_id=_T('target guild id'))
+    @bot_has_permissions(send_messages=True, embed_links=True)
+    @app_commands.default_permissions(administrator=True)
+    @app_commands.guild_only()
+    @owner_only()
+    async def sync_tree(self, interaction: discord.Interaction[Lunaria], guild_id: str | None = None) -> None:
+        await interaction.response.defer(ephemeral=True)
+
+        if guild_id is not None and guild_id.isdigit():
+            obj = discord.Object(id=int(guild_id))
+            await self.bot.tree.sync(guild=obj)
+            return
+        synced = await self.bot.tree.sync()
+
+        embed = Embed(description=f'sync tree: {len(synced)}')  # .success()
+        if guild_id is not None:
+            assert embed.description is not None
+            embed.description += f' : `{guild_id}`'
+
+        # refresh application commands model
+        await self.bot.tree.insert_model_to_commands()
+
+        await interaction.followup.send(embed=embed, ephemeral=True, silent=True)
+
+    # @load.autocomplete('extension')
+    # @unload.autocomplete('extension')
+    # @reload_.autocomplete('extension')
+    # async def tags_autocomplete(self, interaction: Interaction, current: str) -> List[app_commands.Choice[str]]:
+    #     """Autocomplete for extension names."""
+    #
+    #     if interaction.user.id != self.bot.owner_id:
+    #         return [
+    #             app_commands.Choice(name='Only owner can use this command', value='Owner only can use this command')]
+    #
+    #     cogs = [extension.lower() for extension in self.bot._initial_extensions if extension.lower() != 'cogs.admin']
+    #     return [app_commands.Choice(name=cog, value=cog) for cog in cogs]
+
+
+async def setup(bot: Lunaria) -> None:
+    await bot.add_cog(Admin(bot), guild=discord.Object(id=bot.support_guild_id))
---
+++ cogs/jsk.py
@@ -0,0 +1,237 @@
+from __future__ import annotations
+
+import inspect
+import io
+import logging
+import pathlib
+from typing import TYPE_CHECKING, Any, Callable
+
+import discord
+from discord import app_commands
+from discord.app_commands import locale_str as _T
+from discord.app_commands.checks import bot_has_permissions
+from discord.ext import commands
+from discord.ext.commands.view import StringView
+from jishaku.codeblocks import Codeblock, codeblock_converter
+from jishaku.cog import OPTIONAL_FEATURES, STANDARD_FEATURES
+from jishaku.exception_handling import ReplResponseReactor
+from jishaku.features.baseclass import Feature
+from jishaku.functools import AsyncSender
+from jishaku.paginators import PaginatorInterface, WrappedPaginator, use_file_check
+from jishaku.repl import AsyncCodeExecutor, get_var_dict_from_ctx  # type: ignore
+
+from core.checks import owner_only
+
+if TYPE_CHECKING:
+    from core.bot import Lunaria
+
+_log = logging.getLogger(__file__)
+
+# https://github.com/Gorialis/jishaku
+
+
+class Jishaku(*OPTIONAL_FEATURES, *STANDARD_FEATURES, name='jishaku'):
+    bot: Lunaria
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.ctx_msg_jsk_py = app_commands.ContextMenu(
+            name=_T('Python'),
+            callback=self.ctx_message_jishaku_python,
+            guild_ids=[self.bot.support_guild_id],
+        )
+        self.ctx_msg_jsk_py.on_error = self.cog_app_command_error
+        setattr(self.ctx_msg_jsk_py, '__binding__', self)
+
+    async def cog_load(self) -> None:
+        self.bot.tree.add_command(self.ctx_msg_jsk_py)
+        await super().cog_load()
+
+    async def cog_unload(self) -> None:
+        self.bot.tree.remove_command(self.ctx_msg_jsk_py.name, type=self.ctx_msg_jsk_py.type)
+        await super().cog_unload()
+
+    async def cog_app_command_error(
+        self, interaction: discord.Interaction[Lunaria], error: app_commands.AppCommandError
+    ) -> None:
+        interaction.client.dispatch('app_command_error', interaction, error)
+
+    async def cog_check(self, ctx: commands.Context[Lunaria]):
+        if not await ctx.bot.is_owner(ctx.author):
+            raise commands.NotOwner('You must own this bot to use Jishaku.')
+        return True
+
+    async def interaction_check(self, interaction: discord.Interaction[Lunaria]) -> bool:
+        if not await interaction.client.is_owner(interaction.user):
+            raise app_commands.AppCommandError('You must own this bot to use Jishaku.')
+        return super().interaction_check(interaction)
+
+    @Feature.Command(name='jishaku', aliases=['jsk'], invoke_without_command=True, ignore_extra=False)
+    async def jsk(self, ctx: commands.Context[Lunaria]) -> None:
+        """
+        The Jishaku debug and diagnostic commands.
+
+        This command on its own gives a status brief.
+        All other functionality is within its subcommands.
+        """
+        embed = discord.Embed(
+            title='Jishaku',
+            description=f'Jishaku is a debug and diagnostic cog for **{self.bot.user}**.',
+            color=discord.Color.blurple(),
+        )
+        await ctx.send(embed=embed, silent=True)
+
+    @Feature.Command(parent='jsk', name='source', aliases=['src'])
+    async def jsk_source(self, ctx: commands.Context[Lunaria], *, command_name: str) -> None:
+        """
+        Displays the source code for an app command.
+        """
+
+        command = self.bot.get_command(command_name) or self.bot.tree.get_command(command_name)
+        if not command:
+            await ctx.send(f'Couldn\'t find command `{command_name}`.')
+            return
+
+        try:
+            source_lines, _ = inspect.getsourcelines(command.callback)  # type: ignore
+        except (TypeError, OSError):
+            await ctx.send(f'Was unable to retrieve the source for `{command}` for some reason.')
+            return
+
+        filename = 'source.py'
+
+        try:
+            filename = pathlib.Path(inspect.getfile(command.callback)).name  # type: ignore
+        except (TypeError, OSError):
+            pass
+
+        # getsourcelines for some reason returns WITH line endings
+        source_text = ''.join(source_lines)
+
+        if use_file_check(ctx, len(source_text)):  # File "full content" preview limit
+            await ctx.send(file=discord.File(filename=filename, fp=io.BytesIO(source_text.encode('utf-8'))))
+        else:
+            paginator = WrappedPaginator(prefix='```py', suffix='```', max_size=1900)
+
+            paginator.add_line(source_text.replace('```', '``\N{zero width space}`'))
+
+            interface = PaginatorInterface(ctx.bot, paginator, owner=ctx.author)
+            await interface.send_to(ctx)
+
+    @Feature.Command(parent='jsk', name='py', aliases=['python'])
+    async def jsk_python(self, ctx: commands.Context[LatteMaid], *, argument: codeblock_converter):  # type: ignore
+        if TYPE_CHECKING:
+            argument: Codeblock = argument
+
+        arg_dict = get_var_dict_from_ctx(ctx, '')
+        arg_dict.update(get_var_dict_from_ctx(ctx, '_'))
+        arg_dict['_'] = self.last_result
+
+        scope = self.scope
+
+        try:
+            async with ReplResponseReactor(ctx.message):
+                with self.submit(ctx):
+                    executor = AsyncCodeExecutor(argument.content, scope, arg_dict=arg_dict)
+                    async for send, result in AsyncSender(executor):  # type: ignore
+                        send: Callable[..., None]
+                        result: Any
+
+                        if result is None:
+                            continue
+
+                        self.last_result = result
+
+                        send(await self.jsk_python_result_handling(ctx, result))
+
+        finally:
+            scope.clear_intersection(arg_dict)
+
+    @app_commands.command(name=_T('jsk'))
+    @app_commands.describe(sub=_T('Sub command of jsk'), args=_T('Arguments of jsk'))
+    @app_commands.rename(sub=_T('sub'), args=_T('args'))
+    @app_commands.default_permissions(administrator=True)
+    @bot_has_permissions(send_messages=True, embed_links=True, add_reactions=True)
+    @owner_only()
+    async def jishaku_app(
+        self,
+        interaction: discord.Interaction[Lunaria],
+        sub: app_commands.Range[str, 1, 20] | None = None,
+        args: str | None = None,
+    ) -> None:
+        """Jishaku
+
+        Attributes:
+            sub (str): The subcommand to use.
+            args (str): The arguments to pass to the subcommand.
+        """
+
+        await interaction.response.defer(ephemeral=True)
+
+        jsk = self.bot.get_command('jishaku' if sub is None else f'jishaku {sub}')
+
+        if jsk is None:
+            raise app_commands.AppCommandError(f'Couldn\'t find command `jishaku {sub}`.')
+
+        ctx = await self.bot.get_context(interaction)
+        ctx.invoked_with = jsk.qualified_name
+        ctx.command = jsk
+        if args:
+            ctx.view = StringView(args)
+        await self.bot.invoke(ctx)
+
+    @jishaku_app.autocomplete('sub')
+    async def jishaku_app_autocomplete(
+        self,
+        interaction: discord.Interaction,
+        current: str,
+    ) -> list[app_commands.Choice[str]]:
+        subs: list[str] = []
+
+        for command in self.walk_commands():
+            if command == self.jsk:
+                continue
+
+            subs.append(command.qualified_name)
+            subs.extend(command.aliases)
+
+        # remove parent command name
+        subs = [sub.removeprefix('jishaku ') for sub in sorted(subs)]
+
+        if not current:
+            return [app_commands.Choice(name=sub, value=sub.lower()) for sub in subs][:25]
+
+        return [app_commands.Choice(name=sub, value=sub.lower()) for sub in subs if current.lower() in sub.lower()][:25]
+
+    @app_commands.default_permissions(administrator=True)
+    @bot_has_permissions(send_messages=True, embed_links=True, add_reactions=True)
+    @owner_only()
+    async def ctx_message_jishaku_python(
+        self,
+        interaction: discord.Interaction[Lunaria],
+        message: discord.Message,
+    ) -> None:
+        if not message.content:
+            raise app_commands.AppCommandError('No code provided.')
+
+        await interaction.response.defer(ephemeral=message.content.startswith('_'))
+
+        content = message.content.removeprefix('_').strip()
+
+        jsk = self.bot.get_command('jishaku py')
+        ctx = await self.bot.get_context(interaction)
+        codeblock = codeblock_converter(content)
+
+        try:
+            await jsk(ctx, argument=codeblock)  # type: ignore
+        except Exception as e:
+            _log.error(e)
+            # TODO: better error handling
+            raise app_commands.AppCommandError('Invalid Python code.') from e
+
+
+async def setup(bot: Lunaria) -> None:
+    if bot.support_guild_id is not None:
+        await bot.add_cog(Jishaku(bot=bot), guilds=[discord.Object(id=bot.support_guild_id)])
+    else:
+        _log.warning('support guild id is not set. Jishaku cog will not be loaded.')
