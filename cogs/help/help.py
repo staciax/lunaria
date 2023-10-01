1f74bdd0fcc105ace59109b3b3e51227cd783d47
STACiA <stacia.dev@gmail.com>
2023-10-02 00:00:40 +0700

    Update help.py

--- cogs/help/help.py
+++ cogs/help/help.py
@@ -1,24 +1,20 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING  # , Any
+from typing import TYPE_CHECKING, Any
 
 import discord
-from discord import app_commands  # , ui
-from discord import Embed
+from discord import Embed, app_commands, ui
 from discord.app_commands import locale_str as _T
-
-# from discord.app_commands.commands import Command, ContextMenu, Group
-# from discord.app_commands.models import AppCommand
+from discord.app_commands.commands import Command, ContextMenu, Group
+from discord.app_commands.models import AppCommand
 from discord.ext import commands
 
-from core.checks import bot_has_permissions, cooldown_short, dynamic_cooldown  # , user as user_check
+from core.checks import bot_has_permissions, cooldown_short, dynamic_cooldown, user as user_check
 from core.cog import LunaCog as Cog
 from core.i18n import I18n, cog_i18n
 from core.ui.embed import Embed
-
-# from core.ui.view import ViewAuthor
-
-# from core.utils.pages import LattePages, ListPageSource
+from core.ui.view import ViewAuthor
+from core.utils.paginator import ListPageSource, LunaPages
 
 if TYPE_CHECKING:
     from core.bot import Lunaria
@@ -51,181 +47,181 @@ def cog_embed(cog: commands.Cog | Cog, locale: discord.Locale) -> Embed:
     return embed
 
 
-# class HelpPageSource(ListPageSource):
-#     def __init__(self, cog: commands.Cog | Cog, source: list[Command[Any, ..., Any] | Group]) -> None:
-#         super().__init__(sorted(source, key=lambda c: c.qualified_name), per_page=6)
-#         self.cog = cog
+class HelpPageSource(ListPageSource):
+    def __init__(self, cog: commands.Cog | Cog, source: list[Command[Any, ..., Any] | Group]) -> None:
+        super().__init__(sorted(source, key=lambda c: c.qualified_name), per_page=6)
+        self.cog = cog
 
-#     def format_page(
-#         self,
-#         menu: HelpCommandView,
-#         entries: list[Command[Any, ..., Any] | Group],
-#     ) -> Embed:
-#         embed = cog_embed(self.cog, menu.locale)
-#         assert embed.description is not None
-#         for command in entries:
-#             name = command.qualified_name
-#             description = command.description
+    def format_page(
+        self,
+        menu: HelpCommandView,
+        entries: list[Command[Any, ..., Any] | Group],
+    ) -> Embed:
+        embed = cog_embed(self.cog, menu.locale)
+        assert embed.description is not None
+        for command in entries:
+            name = command.qualified_name
+            description = command.description
 
-#             model: AppCommand | None = command.extras.get('model', None)
-#             if model is not None:
-#                 assert isinstance(model, AppCommand)
-#                 name = model.mention
-#                 description = model.description_localizations.get(menu.locale, description)
+            model: AppCommand | None = command.extras.get('model', None)
+            if model is not None:
+                assert isinstance(model, AppCommand)
+                name = model.mention
+                description = model.description_localizations.get(menu.locale, description)
 
-#             embed.description += f'\n{name} - {description}'
+            embed.description += f'\n{name} - {description}'
 
-#         return embed
-
-
-# class CogButton(ui.Button['HelpCommandView']):
-#     def __init__(self, cog: commands.Cog | Cog, entries: list[Command[Any, ..., Any] | Group], *args, **kwargs) -> None:
-#         super().__init__(emoji=getattr(cog, 'display_emoji'), style=discord.ButtonStyle.primary, *args, **kwargs)
-#         self.cog = cog
-#         self.entries = entries
-#         if self.emoji is None:
-#             self.label = cog.qualified_name
-
-#     async def callback(self, interaction: discord.Interaction[Lunaria]) -> None:
-#         assert self.view is not None
-# self.view.source = HelpPageSource(self.cog, self.entries)
-
-# max_pages = self.view.source.get_max_pages()
-# if max_pages > 1:
-#     self.view._add_nav_buttons()
-# else:
-#     self.view._remove_nav_buttons()
-
-# self.disabled = True
-# for child in self.view.children:
-#     if isinstance(child, CogButton) and child != self:
-#         child.disabled = False
-
-# self.view.home_button.disabled = False
-# await self.view.show_page(interaction, 0)
+        return embed
 
 
-# class HelpCommandView(ViewAuthor, LattePages):
-#     def __init__(self, interaction: discord.Interaction[Lunaria], allowed_cogs: tuple[str, ...]):
-#         super().__init__(interaction=interaction, timeout=60.0 * 30)  # 30 minutes
-#         self.allowed_cogs = allowed_cogs
-#         self.embed: Embed = help_command_embed(interaction)
-#         self.cooldown = commands.CooldownMapping.from_cooldown(8.0, 15.0, user_check)  # overide default cooldown
-#         self.go_to_last_page.row = self.go_to_first_page.row = self.go_to_previous_page.row = self.go_to_next_page.row = 1
-#         self.clear_items()
-#         self.add_item(self.home_button)
+class CogButton(ui.Button['HelpCommandView']):
+    def __init__(self, cog: commands.Cog | Cog, entries: list[Command[Any, ..., Any] | Group], *args, **kwargs) -> None:
+        super().__init__(emoji=getattr(cog, 'display_emoji'), style=discord.ButtonStyle.primary, *args, **kwargs)
+        self.cog = cog
+        self.entries = entries
+        if self.emoji is None:
+            self.label = cog.qualified_name
 
-#     def _update_labels(self, page_number: int) -> None:
-#         super()._update_labels(page_number)
-#         self.go_to_next_page.label = 'next'
-#         self.go_to_previous_page.label = 'prev'
-#         # TODO: i18n in lattepages class
+    async def callback(self, interaction: discord.Interaction[Lunaria]) -> None:
+        assert self.view is not None
+        self.view.source = HelpPageSource(self.cog, self.entries)
 
-#     def _add_nav_buttons(self) -> None:
-#         self.add_item(self.go_to_first_page)
-#         self.add_item(self.go_to_previous_page)
-#         self.add_item(self.go_to_next_page)
-#         self.add_item(self.go_to_last_page)
+        max_pages = self.view.source.get_max_pages()
+        if max_pages > 1:
+            self.view._add_nav_buttons()
+        else:
+            self.view._remove_nav_buttons()
 
-#     def _remove_nav_buttons(self) -> None:
-#         self.remove_item(self.go_to_first_page)
-#         self.remove_item(self.go_to_previous_page)
-#         self.remove_item(self.go_to_next_page)
-#         self.remove_item(self.go_to_last_page)
+        self.disabled = True
+        for child in self.view.children:
+            if isinstance(child, CogButton) and child != self:
+                child.disabled = False
 
-#     async def _build_cog_buttons(self) -> None:
-#         user = self.interaction.user
-#         channel = self.interaction.channel
+        self.view.home_button.disabled = False
+        await self.view.show_page(interaction, 0)
 
-#         async def command_available(command: Command[Any, ..., Any] | Group | ContextMenu) -> bool:
-#             # it fine my bot is not nsfw
-#             # if (
-#             #     command.nsfw
-#             #     and channel is not None
-#             #     and not isinstance(channel, (discord.GroupChannel, discord.DMChannel))
-#             #     and not channel.is_nsfw()
-#             # ):
-#             #     return False
 
-#             if isinstance(command, Group):
-#                 return False
+class HelpCommandView(ViewAuthor, LunaPages):
+    def __init__(self, interaction: discord.Interaction[Lunaria], allowed_cogs: tuple[str, ...]):
+        super().__init__(interaction=interaction, timeout=60.0 * 30)  # 30 minutes
+        self.allowed_cogs = allowed_cogs
+        self.embed: Embed = help_command_embed(interaction)
+        self.cooldown = commands.CooldownMapping.from_cooldown(8.0, 15.0, user_check)  # overide default cooldown
+        self.go_to_last_page.row = self.go_to_first_page.row = self.go_to_previous_page.row = self.go_to_next_page.row = 1
+        self.clear_items()
+        self.add_item(self.home_button)
 
-#             if await self.bot.is_owner(user):
-#                 return True
+    def _update_labels(self, page_number: int) -> None:
+        super()._update_labels(page_number)
+        self.go_to_next_page.label = 'next'
+        self.go_to_previous_page.label = 'prev'
+        # TODO: i18n in lattepages class
 
-#             if command._guild_ids:
-#                 return False
+    def _add_nav_buttons(self) -> None:
+        self.add_item(self.go_to_first_page)
+        self.add_item(self.go_to_previous_page)
+        self.add_item(self.go_to_next_page)
+        self.add_item(self.go_to_last_page)
 
-#             # ignore slash commands that are not global
-#             if not isinstance(command, ContextMenu) and command.parent and command.parent._guild_ids:
-#                 return False
+    def _remove_nav_buttons(self) -> None:
+        self.remove_item(self.go_to_first_page)
+        self.remove_item(self.go_to_previous_page)
+        self.remove_item(self.go_to_next_page)
+        self.remove_item(self.go_to_last_page)
 
-#             # ignore slash commands you can't run
-#             if command.checks and not await discord.utils.async_all(f(self.interaction) for f in command.checks):
-#                 return False
+    async def _build_cog_buttons(self) -> None:
+        user = self.interaction.user
+        channel = self.interaction.channel
 
-#             # ignore slash commands you not have default permissions
-#             if (
-#                 command.default_permissions
-#                 and channel is not None
-#                 and isinstance(user, discord.Member)
-#                 and not channel.permissions_for(user) >= command.default_permissions
-#             ):
-#                 return False
+        async def command_available(command: Command[Any, ..., Any] | Group | ContextMenu) -> bool:
+            # it fine my bot is not nsfw
+            # if (
+            #     command.nsfw
+            #     and channel is not None
+            #     and not isinstance(channel, (discord.GroupChannel, discord.DMChannel))
+            #     and not channel.is_nsfw()
+            # ):
+            #     return False
 
-#             return True
+            if isinstance(command, Group):
+                return False
 
-#         for cog in sorted(self.bot.cogs.values(), key=lambda c: c.qualified_name.lower()):
-#             if cog.qualified_name.lower() not in self.allowed_cogs:
-#                 continue
+            if await self.bot.is_owner(user):
+                return True
 
-#             if not list(cog.walk_app_commands()):
-#                 continue
+            if command._guild_ids:
+                return False
 
-#             entries = []
-#             for command in cog.walk_app_commands():
-#                 if not await command_available(command):
-#                     continue
-#                 entries.append(command)
+            # ignore slash commands that are not global
+            if not isinstance(command, ContextMenu) and command.parent and command.parent._guild_ids:
+                return False
 
-#             # TODO: implement context menu
-#             # if isinstance(cog, Cog):
-#             #     context_menus = cog.get_context_menus()
-#             #     for menu in context_menus:
-#             #         if not await command_available(menu):
-#             #             continue
-#             #         entries.append(menu)
+            # ignore slash commands you can't run
+            if command.checks and not await discord.utils.async_all(f(self.interaction) for f in command.checks):
+                return False
 
-#             if not entries:
-#                 continue
+            # ignore slash commands you not have default permissions
+            if (
+                command.default_permissions
+                and channel is not None
+                and isinstance(user, discord.Member)
+                and not channel.permissions_for(user) >= command.default_permissions
+            ):
+                return False
 
-#             self.add_item(CogButton(cog, entries))
+            return True
 
-#     async def before_callback(self, interaction: discord.Interaction[LatteMaid]) -> None:
-#         if self.locale == interaction.locale:
-#             return
-#         self.locale = interaction.locale
-#         self.embed = help_command_embed(interaction)
+        for cog in sorted(self.bot.cogs.values(), key=lambda c: c.qualified_name.lower()):
+            if cog.qualified_name.lower() not in self.allowed_cogs:
+                continue
 
-#     async def start(self) -> None:
-#         await self._build_cog_buttons()
-#         await self.interaction.response.send_message(embed=self.embed, view=self)
-#         self.message = await self.interaction.original_response()
+            if not list(cog.walk_app_commands()):
+                continue
 
-#     @ui.button(emoji='🏘️', style=discord.ButtonStyle.primary, disabled=True)
-#     async def home_button(self, interaction: discord.Interaction[LatteMaid], button: ui.Button) -> None:
-#         # disable home button
-#         button.disabled = True
+            entries = []
+            for command in cog.walk_app_commands():
+                if not await command_available(command):
+                    continue
+                entries.append(command)
 
-#         # disable all cog buttons
-#         for child in self.children:
-#             if isinstance(child, CogButton):
-#                 child.disabled = False
+            # TODO: implement context menu
+            # if isinstance(cog, Cog):
+            #     context_menus = cog.get_context_menus()
+            #     for menu in context_menus:
+            #         if not await command_available(menu):
+            #             continue
+            #         entries.append(menu)
 
-#         # remove nav buttons
-#         self._remove_nav_buttons()
+            if not entries:
+                continue
 
-#         await interaction.response.edit_message(embed=self.embed, view=self)
+            self.add_item(CogButton(cog, entries))
+
+    async def before_callback(self, interaction: discord.Interaction[Lunaria]) -> None:
+        if self.locale == interaction.locale:
+            return
+        self.locale = interaction.locale
+        self.embed = help_command_embed(interaction)
+
+    async def start(self) -> None:
+        await self._build_cog_buttons()
+        await self.interaction.response.send_message(embed=self.embed, view=self)
+        self.message = await self.interaction.original_response()
+
+    @ui.button(emoji='🏘️', style=discord.ButtonStyle.primary, disabled=True)
+    async def home_button(self, interaction: discord.Interaction[Lunaria], button: ui.Button) -> None:
+        # disable home button
+        button.disabled = True
+
+        # disable all cog buttons
+        for child in self.children:
+            if isinstance(child, CogButton):
+                child.disabled = False
+
+        # remove nav buttons
+        self._remove_nav_buttons()
+
+        await interaction.response.edit_message(embed=self.embed, view=self)
 
 
 @cog_i18n(_)
@@ -240,5 +236,5 @@ class Help(Cog, name='help'):
     @dynamic_cooldown(cooldown_short)
     async def help_command(self, interaction: discord.Interaction[Lunaria]):
         cogs = ('about', 'valorant')
-        # help_command = HelpCommandView(interaction, cogs)
-        # await help_command.start()
+        help_command = HelpCommandView(interaction, cogs)
+        await help_command.start()
